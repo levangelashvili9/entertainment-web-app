9390bda643177515c39a78388586dcbc03df8505
Levan Gelashvili <levan.gelashvili9@gmail.com>
2022-11-09 01:34:54 +0400

    fixed basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/entertainment-web-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
